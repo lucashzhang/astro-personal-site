737b20f9dc0b32a80577da382962b090b77c41fe
Lucas Zhang <lucas_h_zhang@hotmail.com>
2024-02-09 23:19:46 -0500

    Add robot integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lucaszhang.net",
+  integrations: [robotsTxt()],
+});
