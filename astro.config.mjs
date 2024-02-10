a34fe13e3f2fb5e981129546dd2cac51b6569916
Lucas Zhang <lucas_h_zhang@hotmail.com>
2024-02-10 10:41:34 -0500

    Adding astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import robotsTxt from "astro-robots-txt";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lucaszhang.net",
-  integrations: [robotsTxt()],
+  integrations: [robotsTxt(), compress({
+    Path: ["./dist", "./_astro", "./images"]
+  })]
 });
